30c2659924846be6aa40d055d35d8074ec6c8c91
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-08-06 19:10:43 -0300

    new updates
    new updates

---
+++ CSHARP-COMPLETO-NELIO-ALVES/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace CSHARP_COMPLETO_NELIO_ALVES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
