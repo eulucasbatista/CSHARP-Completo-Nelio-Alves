369433f1c04ea354806e8d64d856901def8b1920
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2024-08-29 23:12:16 -0300

    l

--- ConsoleApp1/Triangulo.cs
+++ ConsoleApp1/Triangulo.cs
@@ -13,6 +13,12 @@ namespace ConsoleApp1
         public double B;
         public double C;
 
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C)); 
+        }
+
     }
 }
 
